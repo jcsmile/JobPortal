89c21eadf942db473e0a4568258599480debf4af
jcsmile <jcsmile@gmail.com>
2024-11-01 16:20:57 -0500

    Add constants.js

--- api/utils/constants.js
+++ api/utils/constants.js
@@ -1,1 +1,11 @@
-const path = require('path');
+const path = require("path");
+
+require("dotenv").config({ path: path.resolve(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+
+console.log(process.env.NODE_ENV);
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    MONGO_URI: process.env.MONGO_URI || "",
+    NODE_ENV: process.env.NODE_ENV || "development",
+    LOG_LEVEL: process.env.LOG_LEVEL || "info"
+};
