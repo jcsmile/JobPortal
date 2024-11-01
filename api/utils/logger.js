3006ba4d0e2f61dbd8420fa74835ffb7b176b1f8
jcsmile <jcsmile@gmail.com>
2024-11-01 16:21:28 -0500

    Add logger.js

--- api/utils/logger.js
+++ api/utils/logger.js
@@ -1,1 +1,18 @@
-const {format, createLogger, transports} = require('winston');
+const { createLogger, format, transports } = require("winston");
+const { LOG_LEVEL } = require("../utils/constants");
+const logger = createLogger({
+  level: LOG_LEVEL,  // Default logging level; change to 'debug' or 'error' as needed
+  format: format.combine(
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()  // Use JSON format for structured logging
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "logs/error.log", level: "error" }),
+    new transports.File({ filename: "logs/combined.log" })
+  ]
+});
+
+module.exports = logger;
